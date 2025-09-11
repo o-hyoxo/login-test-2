2209bfac3c4004ee99729bbd88398126fa963cb7
o-hyoxo <154452000+o-hyoxo@users.noreply.github.com>
2025-09-11 23:37:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,8 +20,8 @@ from webdriver_manager.firefox import GeckoDriverManager
 load_dotenv()
 
 # Fetch API configuration from environment variables
-CHATGPT_API_URL = os.getenv("CHATGPT_API_URL", "https://ai2gpt.xxxx.nyc.mn/v1/chat/completions")
-CHATGPT_API_KEY = os.getenv("CHATGPT_API_KEY", "sk-cf-wxxx2x4x")
+CHATGPT_API_URL = os.getenv("CHATGPT_API_URL", "https://ai2gpt.oiio.nyc.mn/v1/chat/completions")
+CHATGPT_API_KEY = os.getenv("CHATGPT_API_KEY", "sk-cf-wasd2048")
 
 
 def average_of_array(arr):
